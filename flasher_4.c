138e2578672396165a8e9ddd65450e67d7a580d8
Kannan40030 <kannan.s@ucal.com>
2025-09-15 10:59:37 +0530

    ch2

--- flasher_4.c
+++ flasher_4.c
@@ -11,7 +11,7 @@
 //  Date 02/06/2016
 // 	With Short Ckt Protection & Over Voltage Cut OFF.
 //*********************************************************************
-// 					Pinout Details      // 15 sep 25
+// 					Pinout Details      // 15 sep 
 //*********************************************************************
 // Pin No		Functionality						Usage
 //
